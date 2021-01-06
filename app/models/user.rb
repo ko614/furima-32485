fd996a5f309615ac291874014dd98f09e18441c3
ko614 <koheiosawa614@gmail.com>
2021-01-06 18:41:28 +0900

    エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  
+  validates :nickname, presence: true
+  validates :family_name , presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+  has_many :items
+  has_many :buys
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
