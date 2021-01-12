20ea12a07ff6fdc71830a8168c74eda453806dd0
ko614 <koheiosawa614@gmail.com>
2021-01-12 15:53:18 +0900

    コンフリクト解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,9 +21,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
-  # end
+  def show
+      @item = Item.find(params[:id])
+  end
 
   private
 
