07e071dfbc7c00d15e80adce7500e14b566c0a48
ko614 <koheiosawa614@gmail.com>
2021-01-19 15:49:40 +0900

    コード訂正

--- app/controllers/buy_destination_controller.rb
+++ app/controllers/buy_destination_controller.rb
@@ -40,7 +40,7 @@ class BuyDestinationController < ApplicationController
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
   
   def pay_item
